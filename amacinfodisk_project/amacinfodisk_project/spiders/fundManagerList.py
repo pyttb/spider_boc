dabe5fea8f0b08afe68ac9d6c800b453f5525a91
git07061 <yanghaohl@163.com>
2018-01-29 17:14:56 +0800

    change by yh 20180129

--- amacinfodisk_project/amacinfodisk_project/spiders/fundManagerList.py
+++ amacinfodisk_project/amacinfodisk_project/spiders/fundManagerList.py
@@ -37,12 +37,13 @@ class FundmanagerlistSpider(scrapy.Spider):
         'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8'
     }
     payload_all = [
-        {'creditInfo': "isLostContactMechanism"}  # 失联机构
-         ,{'creditInfo': "identification"}    #异常机构
-         , {'creditInfo': "shamStatement"}     #虚假填报
-         , {'creditInfo': "materRialOmission"}   #重大遗漏
-         , {'creditInfo': "eightLine"}   #违反八项规定
-         , {'creditInfo': "badCredit"}  # 相关主题存在不良记录
+        #{'creditInfo': "isLostContactMechanism"}  # 失联机构
+         #,{'creditInfo': "identification"}    #异常机构
+         #, {'creditInfo': "shamStatement"}     #虚假填报
+         #,
+        {'creditInfo': "materRialOmission"}   #重大遗漏
+         #, {'creditInfo': "eightLine"}   #违反八项规定
+         #, {'creditInfo': "badCredit"}  # 相关主题存在不良记录
     ]
     #payload = {'creditInfo': "identification"} # 失联机构
     def start_requests(self):
@@ -79,6 +80,7 @@ class FundmanagerlistSpider(scrapy.Spider):
             for row in rows:
                 detail_url = row.get("url")
                 url_link="http://gs.amac.org.cn/amac-infodisc/res/pof/manager/"+detail_url;
+                #url_link = "http://gs.amac.org.cn/amac-infodisc/res/pof/manager/101000010425.html";
                 row = dict(row,**payload)
                 yield scrapy.Request(url=url_link, method='GET', headers=self.headers,
                                  body=json.dumps({}),
@@ -87,8 +89,13 @@ class FundmanagerlistSpider(scrapy.Spider):
                                  dont_filter=True)
     #获得具体公司链接下的页面，同时拿到上一层具体公司的其他想信息，一起传入item
     def parseFundManagerPublicInfo(self, response):
-        shilian_jigou =""
-        yichang_jigou =""
+        orgInfo1 =""    #失联机构
+        orgInfo2 =""    #异常机构
+        orgInfo3 = ""   #重大遗漏
+        orgInfo4 = ""   #违反八条底线
+        orgInfo5 = ""   #虚假填报
+        orgInfo6 = ""   #不良诚信
+        orgInfo7 = ""   #其他诚信信息
         organization_code =""
         dataSrc =""
         #拿到传入的公司信息
@@ -100,13 +107,27 @@ class FundmanagerlistSpider(scrapy.Spider):
                 list_item.add_value(key, row.get(key))
         #获取诚信信息中失联机构、异常机构、组织机构代码
         if re.search(u"失联机构.*\n.*", response.text):
-            shilian_jigou = re.search(u"失联机构.*\n.*", response.text).group()
+            orgInfo1 = re.search(u"失联机构.*\n.*", response.text).group()
         if re.search(u"异常机构.*\n.*", response.text):
-            yichang_jigou = re.search(u"异常机构.*\n.*", response.text).group()
+            orgInfo2 = re.search(u"异常机构.*\n.*", response.text).group()
+        if re.search(u"重大遗漏.*\n.*", response.text):
+            orgInfo3 = re.search(u"重大遗漏.*\n.*", response.text).group()
+        if re.search(u"违反八条底线.*\n.*", response.text):
+            orgInfo4 = re.search(u"违反八条底线.*\n.*", response.text).group()
+        if re.search(u"虚假填报.*\n.*", response.text):
+            orgInfo5 = re.search(u"虚假填报.*\n.*", response.text).group()
+        if re.search(u"不良诚信.*\n.*", response.text):
+            orgInfo6 = re.search(u"不良诚信.*\n.*", response.text).group()
+        if re.search(u"其他诚信信息.*\n.*", response.text):
+            orgInfo7 = re.search(u"其他诚信信息.*\n.*", response.text).group()
+            #orgInfo7 = re.match( r'(.*) 其他诚信信息 (.*?) .*', response.text).group()
+
         if response.xpath("/html/body/div/div[2]/div/table/tbody/tr[6]/td[2]/text()"):
             organization_code = response.xpath("/html/body/div/div[2]/div/table/tbody/tr[6]/td[2]/text()").extract_first('').strip()
         if response.xpath('//*[@id="complaint1"]/text()'):
             managerName= response.xpath('//*[@id="complaint1"]/text()').extract_first('').strip()
+
+
         qry_date = datetime.datetime.now().date()
         if row.get('creditInfo') == "isLostContactMechanism":
             dataSrc = "失联机构"
@@ -117,13 +138,18 @@ class FundmanagerlistSpider(scrapy.Spider):
         elif row.get('creditInfo') == "materRialOmission":
             dataSrc = "重大遗漏"
         elif row.get('creditInfo') == "eightLine":
-            dataSrc = "违反八项规定"
+            dataSrc = "违反八项底线"
         else:
             dataSrc = "相关主题存在不良记录"
 
         list_item.add_value('qry_date', qry_date)
-        list_item.add_value('shilian_jigou', shilian_jigou)
-        list_item.add_value('yichang_jigou', yichang_jigou)
+        list_item.add_value('orgInfo1', orgInfo1)
+        list_item.add_value('orgInfo2', orgInfo2)
+        list_item.add_value('orgInfo3', orgInfo3)
+        list_item.add_value('orgInfo4', orgInfo4)
+        list_item.add_value('orgInfo5', orgInfo5)
+        list_item.add_value('orgInfo6', orgInfo6)
+        list_item.add_value('orgInfo7', orgInfo7)
         list_item.add_value('organization_code', organization_code)
         list_item.add_value('dataSrc',dataSrc)
         item = list_item.load_item()
