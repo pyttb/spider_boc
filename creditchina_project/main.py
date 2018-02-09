03713998e646260b84f6ea639209906c24888c45
sppsun <sppsun@gmail.com>
2018-02-09 12:20:35 +0800

    add supervisor support

--- creditchina_project/main.py
+++ creditchina_project/main.py
@@ -5,6 +5,6 @@ import sys
 import os
 reload(sys)
 sys.setdefaultencoding('utf-8')
-
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 execute(['scrapy', 'crawl', 'creditchina'])
